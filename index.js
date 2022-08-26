adcbfb80a21277e9df3a39b461146be8b05014f1
DinaDinakaran <dinadinakaran143@gmail.com>
2022-08-26 19:47:52 +0530

    updateed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app= express()
+const cors = require("cors")
 
 require("dotenv").config()
 const connect =require("./connection/Connection")
@@ -8,6 +9,8 @@ const router = require("./Routers/Routers")
 //midelware
 app.use(express.json())
 app.use("/api",router)
+app.use(cors())
+
 const port = process.env.PORT
 
 app.listen(port,()=>{
